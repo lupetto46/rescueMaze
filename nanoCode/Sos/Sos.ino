264c392b193a35567e50b40c7f4bf2fa9954c5c0
Emmanuele Bonfiglio <bonfiglio.karol.p@gmail.com>
2023-04-05 17:02:14 +0200

    MOOOO

--- nanoCode/Sos/Sos.ino
+++ nanoCode/Sos/Sos.ino
@@ -1,26 +1,53 @@
-void setup()
-{
-	Serial.begin(9600);
-	Serial.setTimeout(1);
+
+unsigned int distance;
+
+class IRSensor {
+	private:
+		int pin;
+		int prevReaded;
+
+	public:
+		IRSensor(int Pin) {
+			pin = Pin;
+			prevReaded = analogRead(pin);
+		}
+
+		unsigned int getDistance() {
+			float readed;
+			//Serial.print(i);
+			unsigned int distance;
+			readed = analogRead(pin);
+			//Serial.println(readed);
+			//Serial.println("");
+			readed = readed * (5/1023);
+			//Serial.print("Readed True: "); Serial.println(readed);
+			distance = 13*pow(readed, -1);
+      delay(10);
+			return distance;
+		}
+};
+
+IRSensor sxAv(A0);
+IRSensor dxAv(A1);
+IRSensor dxPt(A2);
+IRSensor sxPt(A3);
+IRSensor avDx(A6);
+IRSensor avSx(A7);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
 }
 
-void printSerial(String input){
-  input+="#";
-	Serial.print(input);
-}
 
-String readSerial(){
-  String readed = Serial.readStringUntil('#');
-	return readed;
-}
-
-void loop()
-{
-	
-  //printSerial();
-  
-	String input = readSerial();
-  printSerial(input);
-  delay(1200);
-	
+void loop() {
+  	// put your main code here, to run repeatedly:
+  //Serial.print("A0: ");Serial.print(sxAv.getDistance());Serial.println("cm");
+  Serial.print("A1: ");Serial.print(dxAv.getDistance());Serial.println("cm");
+  /*Serial.print("A2: ");Serial.print(dxPt.getDistance());Serial.println("cm");
+  Serial.print("A3: ");Serial.print(sxPt.getDistance());Serial.println("cm");
+  Serial.print("A6: ");Serial.print(avDx.getDistance());Serial.println("cm");
+  Serial.print("A7: ");Serial.print(avSx.getDistance());Serial.println("cm");*/
+  Serial.println("---------------");
+  delay(300);
 }
