a989004676fac9512fa419ce68179bbc0ec5e8ca
lupetto46 <lupetto10597@gmail.com>
2023-04-12 19:17:35 +0200

    Algoritmo di navigazione funzionante

--- PythonCode/Movimento.py
+++ PythonCode/Movimento.py
@@ -33,7 +33,7 @@ def getColor(vals: dict):
 	return c, r, g, b
 
 def rotateOf(grades: int):
-	currentGrades = getGyro(sensors.getSensors()) - grades
+	currentGrades = getGyro(getSensors()) - grades
 	
 	
 	if currentGrades >= 360:
@@ -43,17 +43,17 @@ def rotateOf(grades: int):
 	
 	grad = getGyro(sensors.getSensors())
 	
-	error =4
+	error = 8
 	if grades < 0:
 		print("Sinistra")
-		mov.muovi(-10, 10)
+		mov.muovi(-15, 15)
 	else:
 		print("Destra")
-		mov.muovi(10, -10)
+		mov.muovi(15, -15)
 	
 	while not (currentGrades - error < grad < currentGrades + error):
 		grad = getGyro(sensors.getSensors())
-		print(currentGrades, grad)
+		#print(currentGrades, grad)
 	
 	mov.muovi(0, 0)
 	
@@ -62,8 +62,9 @@ vals = sensors.getSensors()
 gyroOffset = getGyro(vals)
 
 maxOut = 20
+setpoint = 3
 	
-pid = PID(1, 0.1, 0.05, setpoint=6, output_limits=(-maxOut, maxOut))
+pid = PID(1, 0.1, 0.05, setpoint=setpoint, output_limits=(-maxOut, maxOut))
 #7.0, 0.4, 2.0
 pid.tunings = (7.0, 0.4, 0.5)
 print("Starting")
@@ -78,41 +79,99 @@ while True:
 		pass
 
 
+def controlloMassimo(val1, val2):
+	if val1 > val2:
+		return -1
+	elif val1 < val2:
+		return 1
+	else:
+		return 0
+
+sx, dx = 0, 0
+
+class every:
+	def __init__(self, number):
+		self.num = number
+		self.curNum = 0
+		self.prevNum = 0
+		
+	def __call__(self, number):
+		self.curNum += 1
+		
+		if self.curNum == self.num:
+			self.prevNum = number
+			self.curNum = 0
+			return number
+		else:
+			return self.prevNum
+
+ev = every(10)
+
+def allinea(sx, dx):
+	return sx == dx
+
+def ruotaDestra():
+	print("Destra")
+	mov.muovi(20, 20)
+	sleep(0.4)
+	mov.muovi(0, 0)
+	rotateOf(90)
+	mov.muovi(20, 20)
+	sleep(1.8)
+	
+def ruotaSinistra():
+	mov.muovi(0, 0)
+	sleep(0.1)
+	mov.muovi(-20, -20)
+	sleep(0.5)
+	rotateOf(-90)
+	mov.muovi(20, 20)
+	sleep(1.8)
+
+def getSensors():
+	valsLoc = sensors.getSensors()
+			
+	return valsLoc
+
 while True:
 	try:
 		start = time.time()
-		vals = sensors.getSensors()
+		vals = getSensors()
 		#print(vals)
 		offset = straightDegree - vals["gyro"]
 		av, dt = getDx(vals)
-		valForPid = (av+dt) / 2
-		pidVal = pid(valForPid)
-		print(valForPid)
+		controllo = ev(controlloMassimo(dt, av))
+		#print(valForPid)
 		# Controllo muri
-		if getDx(vals)[0] > 15 and getDx(vals)[1] > 15: # Destra
-			mov.muovi(20, 20)
-			sleep(0.2)
-			rotateOf(90)
-			straightDegree = sensors.getSensors()["gyro"]
-			mov.muovi(40, 40)
-			sleep(1)
-			pid = PID(1, 0.1, 0.05, setpoint=4, output_limits=(-maxOut, maxOut))
-			#7.0, 0.4, 2.0
-			pid.tunings = (7.0, 0.4, 1.0)
-		elif getAv(vals)[0] < 8 and getAv(vals)[1] < 8: # Muro avanti
+		print(vals)
+		
+		if (getDx(vals)[0] < 12 and getDx(vals)[1] < 12) and (getAv(vals)[0] < 1 and getAv(vals)[1] < 1) and (getSx(vals)[0] < 12 and getSx(vals)[1] < 12): # Tutti Muri
 			mov.muovi(0, 0)
-			sleep(0.4)
-			mov.muovi(0, 0)
-			rotateOf(-90)
-			straightDegree = sensors.getSensors()["gyro"]
-			mov.muovi(40, 40)
-			sleep(1)
-			pid = PID(1, 0.1, 0.05, setpoint=4, output_limits=(-maxOut, maxOut))
-			#7.0, 0.4, 2.0
-			pid.tunings = (7.0, 0.4, 1.0)
+			sleep(0.1)
+			mov.muovi(-20, -20)
+			sleep(0.5)
+			rotateOf(180)
+		elif getDx(vals)[0] > 15 and getDx(vals)[1] > 15: # Destra
+			ruotaDestra()
+		elif (getAv(vals)[0] <= 1 and getAv(vals)[1] <= 1): # Muro avanti
+			ruotaSinistra()
 		else:
-			sx, dx = 20 + pidVal, 20 - pidVal
+			#print(controllo)
+			if setpoint - 3 < av < setpoint + 3:
+				pass 
+			if controllo == 1 and av <= 15:
+				pidVal = pid(av)
+				sx, dx = 20 + pidVal, 20 - pidVal
+			elif controllo == -1 and dt <= 15:
+				pidVal = pid(dt)
+				sx, dx = 20 - pidVal, 20 + pidVal
+			else:
+				pidVal = pid(av)
+				sx, dx = 20 + pidVal, 20 - pidVal
+			
 			mov.muovi(sx, dx)
+		
+		
 		
 		#print("Time:", time.time() - start)
 	except KeyboardInterrupt:
--- PythonCode/Stop.py
+++ PythonCode/Stop.py
@@ -1,41 +1,17 @@
 import RPi_Movement as mov
-import SerialIO as sens
+import SerialIO as sensors
 import time
 
-def sleep(time_in_seconds: float):
-	start = time.time()
-	while time.time() - start < time_in_seconds:
-		sens.getSensors()
+vals = sensors.getSensors()
 
-def rotateOf(grades: int):
-	currentGrades = getGyro(sensors.getSensors()) - (grades - offset)
-	
-	
-	if currentGrades >= 360:
-		currentGrades -= 360
-	elif currentGrades < 0:
-		currentGrades += 360 
-	
-	grad = getGyro(sensors.getSensors())
-	
-	error =4
-	if grades < 0:
-		print("Sinistra")
-		mov.muovi(-10, 10)
-	else:
-		print("Destra")
-		mov.muovi(10, -10)
-	
-	while not (currentGrades - error < grad < currentGrades + error):
-		grad = getGyro(sensors.getSensors())
-	
-	mov.muovi(0, 0)
-
-straightDegree = sens.getSensors()["gyro"]
-offset = straightDegree - sens.getSensors()["gyro"]
+def getSensors():
+	valsLoc = sensors.getSensors()
+	for valname in valsLoc:
+		if not (valname == "color" or valname == "gyro"):
+			if valsLoc[valname] > 30:
+				valsLoc[valname] = 10000
+			
+	return valsLoc
 
 while True:
-	vals = sens.getSensors()
-	
-	print(vals["avDx"] ,vals["dtDx"],(vals["avDx"] + vals["dtDx"]) / 2)
-	
+	print(getSensors())
